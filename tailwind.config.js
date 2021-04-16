bde930100b8b7242e0137e3b8d300fba675a8335
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-04-16 13:54:47 -0700

    adds sans font to replace default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        mono: ["DM Mono"],
+        mono: ["DM Mono", ...defaultTheme.fontFamily.mono],
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans]
       },
     },
   },
